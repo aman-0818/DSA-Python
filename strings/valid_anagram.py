e1ffc56544b1b58e5ba70b3d953989b4afdf405d
Aman Saifi <Mr.aman7248@gmail.com>
2025-11-17 18:30:45 +0530

    Add Valid Anagram solution in strings folder

---
+++ strings/valid_anagram.py
@@ -0,0 +1,20 @@
+# Valid Anagram solution 
+def is_anagram(s, t):
+    if len(s) != len(t):
+        return False
+    
+    count = {}
+
+    for ch in s:
+        count[ch] = count.get(ch, 0) + 1
+
+    for ch in t:
+        if ch not in count or count[ch] == 0:
+            return False
+        count[ch] -= 1
+
+    return True
+
+# Example
+print(is_anagram("anagram", "nagaram"))  # True
+print(is_anagram("rat", "car"))          # False
