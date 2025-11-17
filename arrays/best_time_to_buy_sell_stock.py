92bf3ef535db95ed96be8c60b0d38c867eda81a5
Aman Saifi <Mr.aman7248@gmail.com>
2025-11-17 18:33:11 +0530

    Add Best Time to Buy and Sell Stock solution in arrays folder

---
+++ arrays/best_time_to_buy_sell_stock.py
@@ -0,0 +1,16 @@
+# Best Time to Buy and Sell Stock solution 
+def max_profit(prices):
+    min_price = float('inf')
+    max_profit = 0
+
+    for price in prices:
+        if price < min_price:
+            min_price = price
+        else:
+            max_profit = max(max_profit, price - min_price)
+
+    return max_profit
+
+# Example
+print(max_profit([7, 1, 5, 3, 6, 4]))  # 5
+print(max_profit([7, 6, 4, 3, 1]))      # 0
