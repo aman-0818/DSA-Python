ef2cd201753a6ca056b5bb59c6b3aefa2b474bc2
Aman Saifi <Mr.aman7248@gmail.com>
2025-11-17 18:24:08 +0530

    Add Two Sum problem solution in arrays folder

---
+++ arrays/two_sum.py
@@ -0,0 +1,11 @@
+# Two Sum Solution 
+def two_sum(nums, target):
+    hashmap = {}
+    for i, num in enumerate(nums):
+        diff = target - num
+        if diff in hashmap:
+            return [hashmap[diff], i]
+        hashmap[num] = i
+
+# Example
+print(two_sum([2, 7, 11, 15], 9))
